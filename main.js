692fce74ae149f62af393c12adc93896fe4655ba
GinoGreen <luigiverde97@gmail.com>
2021-10-22 15:33:38 +0200

    commit routine, funzione non funzionante

---
+++ main.js
@@ -0,0 +1,6 @@
+const showMenu = document.querySelector('div.hambuger-menu');
+const toggleMenu = document.querySelectorAll('.header-right a i.fa-bars');
+
+toggleMenu.addEventListener('click', function () {
+   console.log('hai cliccato sull\'hamburger');
+});
