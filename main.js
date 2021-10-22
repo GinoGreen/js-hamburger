589ddcbf379c6992fc438f8cc1cd88495f799977
GinoGreen <luigiverde97@gmail.com>
2021-10-22 15:36:02 +0200

    correzione funzione

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 const showMenu = document.querySelector('div.hambuger-menu');
-const toggleMenu = document.querySelectorAll('.header-right a i.fa-bars');
+const toggleMenu = document.querySelector('.header-right a i.fa-bars');
 
 toggleMenu.addEventListener('click', function () {
    console.log('hai cliccato sull\'hamburger');
