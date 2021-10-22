0c3e5df91b4360a314f3ace7faff850cb7206119
GinoGreen <luigiverde97@gmail.com>
2021-10-22 15:57:19 +0200

    Esercitazione completata

--- main.js
+++ main.js
@@ -1,6 +1,16 @@
-const showMenu = document.querySelector('div.hambuger-menu');
-const toggleMenu = document.querySelector('.header-right a i.fa-bars');
+const showMenu = document.querySelector('div.hamburger-menu');
+const toggleMenuOn = document.querySelector('.header-right .fa-bars');
+const toggleMenuOff = document.querySelector('div.hamburger-menu .close');
 
-toggleMenu.addEventListener('click', function () {
+
+toggleMenuOn.addEventListener('click', function () {
    console.log('hai cliccato sull\'hamburger');
+   console.log(toggleMenuOn.classList);
+   console.log(showMenu.classList);
+   showMenu.classList.add('active');
+});
+
+toggleMenuOff.addEventListener('click', function () {
+   console.log('hai cliccato sulla X di chiusura');
+   showMenu.classList.remove('active');
 });
